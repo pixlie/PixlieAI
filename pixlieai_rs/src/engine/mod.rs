b0b4e422a85b9e6e7f60b733e17047949e659d9a
Sumit Datta <sumitdatta@gmail.com>
2024-11-18 16:13:29 +0530

    Switched to GNU GPL v3.0 across the project

--- pixlieai_rs/src/engine/mod.rs
+++ pixlieai_rs/src/engine/mod.rs
@@ -1,5 +1,5 @@
 // Copyright 2024 Pixlie Web Solutions Pvt. Ltd.
-// Licensed under the Business Source License 1.1 (the "License");
+// Licensed under the GNU General Public License version 3.0;
 // You may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
