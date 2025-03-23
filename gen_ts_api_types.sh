bc9adbb5ab22d4b8ea1d1e37d47e713ee37746f2
Mitesh Ashar <email@miteshashar.com>
2025-03-23 11:33:55 +0530

    Run CI build to check API types failure

--- gen_ts_api_types.sh
+++ gen_ts_api_types.sh
@@ -1,8 +1,18 @@
+# Backup API Types
+[ -e admin/src/api_types_bak ] && rm -r admin/src/api_types_bak
+mv admin/src/api_types admin/src/api_types_bak
+
+# # Generate API Types
 export TS_RS_EXPORT_DIR="../admin/src/api_types"
 cd pixlie_ai && \
-    cargo test && \
-    cd ..
+    if [[ " $@ " =~ " --verbose " ]]; then
+        cargo test --verbose
+    else
+        cargo test
+    fi && \
+    cd .. || (cd .. && mv admin/src/api_types_bak admin/src/api_types && exit 1)
 
-[ -e admin/src/api_types ] && rm -r src/admin/api_types
+# # Format API Types
 cd admin &&
-    pnpm prettier src/api_types --write
+    (pnpm prettier src/api_types --write && rm -r src/api_types_bak) ||
+        (rm -r src/api_types && mv src/api_types_bak src/api_types && exit 1)
