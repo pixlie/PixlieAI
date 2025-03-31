f40bd334e3014709965f1ef2f1366ccae5de5b33
spongebobsmartypants <spongebobsmartypants2000@gmail.com>
2025-03-30 20:13:25 -0400

    more icons

--- admin/src/widgets/interactable/Icon.tsx
+++ admin/src/widgets/interactable/Icon.tsx
@@ -5,15 +5,15 @@ const ICONS: Record<string, () => JSX.Element> = {
   "brand-calendly": () => (
     <img
       src="https://cdn.brandfetch.io/idbJpTKFPT/theme/dark/symbol.svg?c=1dxbfHSJFAPEGdCLU4o5B"
-      class="h-6 w-6"
+      class="h-full w-full"
     />
   ),
   "brand-discord": () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      width="24"
-      height="24"
+      width="100%"
+      height="100%"
       class="main-grid-item-icon"
       fill="none"
     >
@@ -27,8 +27,8 @@ const ICONS: Record<string, () => JSX.Element> = {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      width="24"
-      height="24"
+      width="100%"
+      height="100%"
       class="main-grid-item-icon"
       fill="none"
     >
@@ -41,17 +41,71 @@ const ICONS: Record<string, () => JSX.Element> = {
   //   SHIELD ICONS
   "shield-github-stars": () => (
     <img
-      src="https://img.shields.io/github/stars/pixlie/PixlieAI?style=social"
+      src="https://img.shields.io/github/stars/pixlie/PixlieAI?style=social?"
       alt="GitHub Stars"
+      class="h-full w-auto"
     />
   ),
   // FEATHER ICONS
+  "arrow-up": () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="100%"
+      height="100%"
+      class="main-grid-item-icon"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="3"
+    >
+      <line x1="12" x2="12" y1="19" y2="5" />
+      <polyline points="5 12 12 5 19 12" />
+    </svg>
+  ),
+  "arrow-up-circle": () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="100%"
+      height="100%"
+      class="main-grid-item-icon"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <polyline points="16 12 12 8 8 12" />
+      <line x1="12" x2="12" y1="16" y2="8" />
+    </svg>
+  ),
+
+  bell: () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="100%"
+      height="100%"
+      class="main-grid-item-icon"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
+      <path d="M13.73 21a2 2 0 0 1-3.46 0" />
+    </svg>
+  ),
   check: () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      width="24"
-      height="24"
+      width="100%"
+      height="100%"
       class="main-grid-item-icon"
       fill="none"
       stroke="currentColor"
@@ -62,12 +116,28 @@ const ICONS: Record<string, () => JSX.Element> = {
       <polyline points="20 6 9 17 4 12" />
     </svg>
   ),
+  "chevron-right": () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="100%"
+      height="100%"
+      class="main-grid-item-icon"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <polyline points="9 18 15 12 9 6" />
+    </svg>
+  ),
   "external-link": () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      width="24"
-      height="24"
+      width="100%"
+      height="100%"
       class="main-grid-item-icon"
       fill="none"
       stroke="currentColor"
@@ -80,12 +150,28 @@ const ICONS: Record<string, () => JSX.Element> = {
       <line x1="10" x2="21" y1="14" y2="3" />
     </svg>
   ),
+  github: () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="100%"
+      height="100%"
+      class="main-grid-item-icon"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
+    </svg>
+  ),
   help: () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      width="24"
-      height="24"
+      width="100%"
+      height="100%"
       class="main-grid-item-icon"
       fill="none"
       stroke="currentColor"
@@ -98,12 +184,53 @@ const ICONS: Record<string, () => JSX.Element> = {
       <line x1="12" x2="12.01" y1="17" y2="17" />
     </svg>
   ),
+  info: () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="100%"
+      height="100%"
+      class="main-grid-item-icon"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <line x1="12" x2="12" y1="16" y2="12" />
+      <line x1="12" x2="12.01" y1="8" y2="8" />
+    </svg>
+  ),
+  loader: () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="100%"
+      height="100%"
+      class="main-grid-item-icon"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <line x1="12" x2="12" y1="2" y2="6" />
+      <line x1="12" x2="12" y1="18" y2="22" />
+      <line x1="4.93" x2="7.76" y1="4.93" y2="7.76" />
+      <line x1="16.24" x2="19.07" y1="16.24" y2="19.07" />
+      <line x1="2" x2="6" y1="12" y2="12" />
+      <line x1="18" x2="22" y1="12" y2="12" />
+      <line x1="4.93" x2="7.76" y1="19.07" y2="16.24" />
+      <line x1="16.24" x2="19.07" y1="7.76" y2="4.93" />
+    </svg>
+  ),
   mail: () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      width="24"
-      height="24"
+      width="100%"
+      height="100%"
       class="main-grid-item-icon"
       fill="none"
       stroke="currentColor"
@@ -115,12 +242,30 @@ const ICONS: Record<string, () => JSX.Element> = {
       <polyline points="22,6 12,13 2,6" />
     </svg>
   ),
+  "more-horizontal": () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="100%"
+      height="100%"
+      class="main-grid-item-icon"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <circle cx="12" cy="12" r="1" />
+      <circle cx="19" cy="12" r="1" />
+      <circle cx="5" cy="12" r="1" />
+    </svg>
+  ),
   plus: () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      width="24"
-      height="24"
+      width="100%"
+      height="100%"
       class="main-grid-item-icon"
       fill="none"
       stroke="currentColor"
@@ -132,12 +277,45 @@ const ICONS: Record<string, () => JSX.Element> = {
       <line x1="5" x2="19" y1="12" y2="12" />
     </svg>
   ),
+  play: () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="100%"
+      height="100%"
+      class="main-grid-item-icon"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <polygon points="5 3 19 12 5 21 5 3" />
+    </svg>
+  ),
+  "play-circle": () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="100%"
+      height="100%"
+      class="main-grid-item-icon"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <polygon points="10 8 16 12 10 16 10 8" />
+    </svg>
+  ),
   "plus-circle": () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      width="24"
-      height="24"
+      width="100%"
+      height="100%"
       class="main-grid-item-icon"
       fill="none"
       stroke="currentColor"
@@ -154,8 +332,8 @@ const ICONS: Record<string, () => JSX.Element> = {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      width="24"
-      height="24"
+      width="100%"
+      height="100%"
       class="main-grid-item-icon"
       fill="none"
       stroke="currentColor"
@@ -168,12 +346,46 @@ const ICONS: Record<string, () => JSX.Element> = {
       <polyline points="7 3 7 8 15 8" />
     </svg>
   ),
+  search: () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="100%"
+      height="100%"
+      class="main-grid-item-icon"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <circle cx="11" cy="11" r="8" />
+      <line x1="21" x2="16.65" y1="21" y2="16.65" />
+    </svg>
+  ),
+  send: () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="100%"
+      height="100%"
+      class="main-grid-item-icon"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <line x1="22" x2="11" y1="2" y2="13" />
+      <polygon points="22 2 15 22 11 13 2 9 22 2" />
+    </svg>
+  ),
   settings: () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      width="24"
-      height="24"
+      width="100%"
+      height="100%"
       class="main-grid-item-icon"
       fill="none"
       stroke="currentColor"
@@ -185,15 +397,42 @@ const ICONS: Record<string, () => JSX.Element> = {
       <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z" />
     </svg>
   ),
+  star: () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="100%"
+      height="100%"
+      class="main-grid-item-icon"
+      fill="currentColor"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+    </svg>
+  ),
 };
 
 interface IPropTypes {
   name: string;
+  size?: number;
   color?: string;
+  class?: string;
 }
 
 const Icon: Component<IPropTypes> = (props) => (
-  <div class={`${props.color ?? ""}`}>{ICONS[props.name]()}</div>
+  <div
+    style={{
+      height: props.size ? `${props.size}px` : "24px",
+      width: props.size ? `${props.size}px` : "24px",
+      color: props.color ? props.color : "",
+    }}
+    class={props.class}
+  >
+    {ICONS[props.name]()}
+  </div>
 );
 
 export default Icon;
