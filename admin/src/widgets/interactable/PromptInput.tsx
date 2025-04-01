2e4a7133ad5a043668c305793dd02fc0c19fa61d
spongebobsmartypants <spongebobsmartypants2000@gmail.com>
2025-03-31 23:13:08 -0400

    spacing

--- admin/src/widgets/interactable/PromptInput.tsx
+++ admin/src/widgets/interactable/PromptInput.tsx
@@ -16,7 +16,7 @@ const PromptInput: Component<IFormField> = (props) => {
       id={props.id}
       name={props.name}
       required={props.isRequired !== null ? props.isRequired : false}
-      class={`block w-full rounded-lg px-3 py-2 outline-none font-content m-0 bg-transparent`}
+      class={`block w-full rounded-lg px-2 py-2 outline-none font-content m-0 bg-transparent`}
       placeholder={props.placeholder !== null ? props.placeholder : ""}
       value={props.value || ""}
       onChange={handleChange}
