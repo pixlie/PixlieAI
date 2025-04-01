322022fec40a344d1baf3700d316b9272505000d
spongebobsmartypants <spongebobsmartypants2000@gmail.com>
2025-03-31 21:57:51 -0400

    tool tip improvements

--- admin/src/widgets/navigation/ToolTip.tsx
+++ admin/src/widgets/navigation/ToolTip.tsx
@@ -7,15 +7,10 @@ interface ToolTipProps {
 
 const ToolTip: Component<ToolTipProps> = (props) => {
   return (
-    <div class="relative group">
-        <div>
-      {props.children}
-      </div>
-      <div class="absolute hidden group-hover:block top-full left-1/2 transform -translate-x-1/2 mt-3 px-2 py-1 text-sm text-white bg-gray-900 rounded-lg whitespace-nowrap">
+    <div class="relative group inline-block">
+      <div>{props.children}</div>
+      <div class="pointer-events-none absolute top-full left-1/2 -translate-x-1/2 mt-0.5 px-2 py-1.5 text-sm font-medium text-white bg-gray-900 rounded-md whitespace-nowrap opacity-0 translate-y-1 scale-95 transition duration-150 ease-out group-hover:opacity-100 group-hover:translate-y-0 group-hover:scale-100 z-10">
         {props.text}
-        <div class="absolute bottom-full left-1/2 transform -translate-x-1/2 mt-1">
-          <div class="border-solid border-b-gray-900 border-b-8 border-x-transparent border-x-8 border-t-0" />
-        </div>
       </div>
     </div>
   );
