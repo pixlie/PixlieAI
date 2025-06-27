5a58bf8cb6a88ab2e7c06f88fffcedc20e00abd9
spongebobsmartypants <spongebobsmartypants2000@gmail.com>
2025-06-27 01:25:08 -0400

    undo

--- admin/src/routes/index.tsx
+++ admin/src/routes/index.tsx
@@ -1,13 +1,13 @@
 import { Component } from "solid-js";
 import { Route } from "@solidjs/router";
-import PerProjectWrapper from "./projects/PerProject";
-import HelpContainer from "./help/HelpContainer";
-import Contact from "./help/Contact";
-import Terminal from "./projects/Terminal.tsx";
 import Workflow from "./projects/Workflow";
 import Insights from "./projects/Insights";
 import Crawl from "./projects/Crawl";
 import Search from "./projects/Search";
+import PerProjectWrapper from "./projects/PerProject";
+import HelpContainer from "./help/HelpContainer";
+import Contact from "./help/Contact";
+import CreateProject from "./projects/CreateProject.tsx";
 import Graph from "./projects/Graph.tsx";
 import Explorer from "./projects/Explorer.tsx";
 import Results from "./projects/Results.tsx";
@@ -16,11 +16,11 @@ const Routes: Component = () => {
   return (
     <>
       <Route path="/">
-        <Route path="" component={Terminal} />
+        <Route path="" component={CreateProject} />
 
-        <Route path="/p" component={PerProjectWrapper}>
-          <Route path="/:projectId">
-            <Route path="" component={Terminal} />
+        <Route path="/p">
+          <Route path="/create" component={CreateProject} />
+          <Route path="/:projectId" component={PerProjectWrapper}>
             <Route path="/workflow" component={Workflow} />
             <Route path="/explorer" component={Explorer} />
             <Route path="/insights" component={Insights} />
